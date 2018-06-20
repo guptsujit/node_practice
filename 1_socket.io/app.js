2c22017d5f7d7b183534774149bc43c26fb06e22
guptsujit <guptsujit@gmail.com>
2018-06-20 22:23:39 +0530

    Added code for if user starts typing a chat message

--- 1_socket.io/app.js
+++ 1_socket.io/app.js
@@ -15,18 +15,27 @@ const io = new socketIO(server); // works with new or just use socketIO(server)
 io.on('connection', (socket) => {
     console.log('someone is connected');
 
-    socket.on('newMessage', (message,username) => {
-       //With broadcast the user who will send the message will not see his/her own message at his end
-       // But other user will receive the message 
-       //socket.broadcast.emit('sendMessage', message,username);
+    socket.on('newMessage', (message, username) => {
+        //With broadcast the user who will send the message will not see his/her own message at his end
+        // But other user will receive the message 
+        //socket.broadcast.emit('sendMessage', message,username);
         //with socket.emit message will send only to a curent connection that is the 
         //user who will send the message will not deliver to other user but the message will be send to himself
-    // socket.emit('sendMessage', message,username);
-       // io.emit will send the message to all the connected user
-        io.emit('sendMessage', message,username);
+        // socket.emit('sendMessage', message,username);
+        // io.emit will send the message to all the connected user
+        io.emit('sendMessage', message, username, true);
+
     })
 
 
+    socket.on('typing', (message) => {
+        //emit event user is typing
+        socket.broadcast.emit('usertyping', message);
+    })
+    socket.on('typingStopped', (condition) => {
+        //emit event user is typing
+        socket.broadcast.emit('userStoppedTyping', condition);
+    })
     socket.on('disconnect', () => {
         console.log('someone is disconnected from server');
     })
