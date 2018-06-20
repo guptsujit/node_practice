4f48da3ec78baac39053a07bc02fe14b9bfd22eb
guptsujit <guptsujit@gmail.com>
2018-06-20 19:34:12 +0530

    updated app.js for chatbox

--- 1_socket.io/app.js
+++ 1_socket.io/app.js
@@ -16,8 +16,14 @@ io.on('connection', (socket) => {
     console.log('someone is connected');
 
     socket.on('newMessage', (message,username) => {
-        socket.broadcast.emit('sendMessage', message,username);
-
+       //With broadcast the user who will send the message will not see his/her own message at his end
+       // But other user will receive the message 
+       //socket.broadcast.emit('sendMessage', message,username);
+        //with socket.emit message will send only to a curent connection that is the 
+        //user who will send the message will not deliver to other user but the message will be send to himself
+    // socket.emit('sendMessage', message,username);
+       // io.emit will send the message to all the connected user
+        io.emit('sendMessage', message,username);
     })
 
 
